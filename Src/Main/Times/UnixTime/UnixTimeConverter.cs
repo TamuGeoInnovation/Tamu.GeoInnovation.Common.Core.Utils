f9da47ceb7c8de85bb8ab7d066b8a1df982106c1
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2017-06-05 19:21:51 -0500

    added linux time converter

---
+++ Src/Main/Times/UnixTime/UnixTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamu.GeoInnovation.Common.Core.Utils.Times.UnixTime
+{
+    public class UnixTimeConverter
+    {
+
+        public static DateTime FromUnixTime(long unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+
+
+        public static DateTime FromUnixTimeSeconds(double unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+
+        public static DateTime FromUnixTimeMilliseconds(double unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddMilliseconds(unixTime);
+        }
+    }
+}
