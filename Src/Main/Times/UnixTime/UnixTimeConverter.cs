48e755685591fd2b3a2e31ea027881ce919ab81b
pbaldridge <paytonbaldridge@gmail.com>
2017-07-03 10:01:23 -0500

    daily push

---
+++ Src/Main/Times/UnixTime/UnixTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Tamu.GeoInnovation.Common.Core.Utils.Times.UnixTime
+{
+    public class UnixTimeConverter
+    {
+
+        public static DateTime FromUnixTime(long unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+
+
+        public static DateTime FromUnixTimeSeconds(double unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddSeconds(unixTime);
+        }
+
+        public static DateTime FromUnixTimeMilliseconds(double unixTime)
+        {
+            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            return epoch.AddMilliseconds(unixTime);
+        }
+    }
+}
