9140370195ad1f1c36cf50ae3e64ce0227a77210
danielgoldbergtamu <daniel.goldberg@tamu.edu>
2018-04-05 14:53:42 -0500

    removed OrdinalWhereClause into core.addresses

--- Src/Main/Utils/Numbers/NumberUtils.cs
+++ Src/Main/Utils/Numbers/NumberUtils.cs
@@ -1,9 +1,5 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using USC.GISResearchLab.Common.Addresses;
-using USC.GISResearchLab.Common.Core.TextEncodings.Soundex;
 
 namespace USC.GISResearchLab.Common.Utils.Numbers
 {
@@ -137,113 +133,7 @@ namespace USC.GISResearchLab.Common.Utils.Numbers
                             "quadrillion",
                             "quintillion"
                           };
-        public class OrdinalWhereClause
-        {
-            public string WhereClause { get; set; }
-            //public string OrdParam1 { get; set; }
-            //public string OrdParam2 { get; set; }
-            //public string OrdParam3 { get; set; }
-            public List<KeyValuePair<string,string>> OrdParams { get; set; }
-
-            public OrdinalWhereClause()
-            {
-                WhereClause = "";
-                OrdParams = new List<KeyValuePair<string, string>> { };
-
-            }
-            public OrdinalWhereClause(string whereClause, List<KeyValuePair<string, string>> ordParams)
-            {
-                WhereClause = whereClause;
-                OrdParams = ordParams;
-            }
-            //Other properties, methods, events...
-        }
-        //Payton: If streetname is numeric this adds where clause to account for multiple variations of the name
-        public static OrdinalWhereClause getOrdinalWhereClause(StreetAddress streetAddress, string streetSoundexFieldName)
-        {
-            OrdinalWhereClause ret = new OrdinalWhereClause();
-            StringBuilder where = new StringBuilder();
-            StringBuilder parms = new StringBuilder();
-            
-
-            if (!String.IsNullOrEmpty(streetAddress.PreType)) // Via De La Vialla
-            {
-                where.Append("			OR ");
-                where.Append("			"+ streetSoundexFieldName + "=@ordParam1 ");
-            }
-            else if (streetAddress.StreetName.ToUpper().StartsWith("SAINT")) // Saint, add in ST
-            {
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam1 ");
-            }
-            else if (streetAddress.NameIsNumericAbbreviation) // 1st street, add in first soundex, and 1 soundex
-            {
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam1 ");
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam2 ");
-
-            }
-            else if (streetAddress.NameIsNumberWords) // first street, add in 1st soundex, and 1 soundex
-            {
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam2 ");
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam3 ");
-            }
-            else if (streetAddress.NameIsNumber) // 1, add in 1st soundex, first soundex, and 1 soundex
-            {
-
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam1 ");
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam2 ");
-                where.Append("			OR ");
-                where.Append("			" + streetSoundexFieldName + "=@ordParam3 ");
-            }
-
-            ret.WhereClause = where.ToString();
-
-            if (!String.IsNullOrEmpty(streetAddress.PreType)) // Via De La Vialla
-            {
-                ret.OrdParams.Add(new KeyValuePair<string,string>("ordParam1", SoundexEncoder.ComputeEncodingNew(streetAddress.PreType + " " + streetAddress.StreetName)));
-            }
-            else if (streetAddress.StreetName.ToUpper().StartsWith("SAINT")) // Saint, add in ST
-            {                
-                string newName = streetAddress.StreetName.ToUpper().Replace("SAINT", "ST");
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam1", SoundexEncoder.ComputeEncodingNew(newName)));             
-            }
-            else if (streetAddress.NameIsNumericAbbreviation) // 1st street, add in first soundex and 1 soundex
-            {
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam1", SoundexEncoder.ComputeEncodingNew(NumberUtils.IntegerToWords(streetAddress.StreetName, true))));
-
-                int number = NumberUtils.GetNumberPartOfNumericAbbreviation(streetAddress.StreetName);
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam2", SoundexEncoder.ComputeEncodingNew(number.ToString())));
-            }
-            else if (streetAddress.NameIsNumberWords) // first street, add in 1st soundex and 1 soundex
-            {
-                string numericAbbreivation = NumberUtils.WordsToNumericAbbreviation(streetAddress.StreetName);
-                int number = NumberUtils.GetNumberPartOfNumericAbbreviation(numericAbbreivation);
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam2", SoundexEncoder.ComputeEncodingNew(numericAbbreivation)));
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam3", SoundexEncoder.ComputeEncodingNew(number.ToString())));
-            }
-            else if (streetAddress.NameIsNumber) // 1, add in 1st soundex, first soundex, and 1 soundex
-            {
-                string integerWords = NumberUtils.IntegerToWords(streetAddress.StreetName, true);
-                string integerWordsSoundex = SoundexEncoder.ComputeEncodingNew(integerWords);
-
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam1", integerWordsSoundex));
-
-                int number = Convert.ToInt32(streetAddress.StreetName);
-                string numericAbbreviation = NumberUtils.getNumericAbbreviationSuffixForNumber(number);
-                string numericAbbreviationSoundex = SoundexEncoder.ComputeEncodingNew(streetAddress.StreetName + numericAbbreviation);
-
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam2", numericAbbreviationSoundex));
-                ret.OrdParams.Add(new KeyValuePair<string, string>("ordParam3", SoundexEncoder.ComputeEncodingNew(number.ToString())));
-            }
-            return ret;
-        }
-
+        
         public static bool IsInt(object o)
         {
             return IsInt(o.ToString());
